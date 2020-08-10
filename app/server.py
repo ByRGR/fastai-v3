9bca2c4ca89e2e2350958e5d08baf57169c96629
ByRGR <69435093+ByRGR@users.noreply.github.com>
2020-08-10 12:14:40 +0200

    Update of server.py
    new classes: 
    bmw
    mercedes
    vw
    (certain models only)

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=10fiibIo6UR3K4K_LM_Mdpq0tL-7fDNlz'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['bmw', 'mercedes', 'vw']
 path = Path(__file__).parent
 
 app = Starlette()
